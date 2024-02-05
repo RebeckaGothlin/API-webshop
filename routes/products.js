00fc14496eb9176bec47097bbae0f331048ab5f4
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-02-05 11:31:22 +0100

    :sparkles: add find specific product

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { ObjectId } = require('mongodb');
 var router = express.Router();
 
 /* GET home page. */
@@ -13,6 +14,23 @@ router.get('/', function(req, res, next) {
         res.status(500).json({error: 'Internal Server Error'});
     })
 });
+
+router.get('/:id', (req,res) => {    
+    const productId = req.params.id;
+
+    req.app.locals.db.collection('products').findOne({ _id: new ObjectId(productId)})
+        .then(product => {
+            if (!product) {
+                res.status(404).json({ error: 'Produkten kan inte hittas'})
+                return;
+            }
+            res.status(200).json(product);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({error: 'Internal Server Error'});
+        })
+})
 
 
 module.exports = router;
