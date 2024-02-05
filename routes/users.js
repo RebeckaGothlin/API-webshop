f942213d7f079fe3a8b2d349284f9d8d89b102c3
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-02-05 14:37:47 +0100

    :sparkle: add get spec user POST

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { ObjectId } = require('mongodb');
 var router = express.Router();
 
 /* GET users listing. */
@@ -13,6 +14,25 @@ router.get('/', function(req, res, next) {
     res.status(500).json({error: 'Internal Server Error'});
   })
 });
+
+
+router.post('/', async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const user = await req.app.locals.db.collection('users').findOne({ _id: new ObjectId(userId)});
+
+    if (!user) {
+      res.status(404).json({error: 'Användaren kan inte hittas'})
+      return;
+    }
+
+    res.json(user)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error'})
+  }
+})
+
 
 // Skapa user 
 router.post('/add', (req, res) => {
