7780fe2a97bbd1d9cde11407b2be3a81708a577f
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-02-05 09:07:59 +0100

    :sparkles: get req user endpoint

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  req.app.locals.db.collection('users').find({}, { projection: { password: 0}}).toArray()
+  .then(results => {
+    console.log(results);
+    res.json(results);
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).json({error: 'Internal Server Error'});
+  })
+});
+
+router.get('/:id', function(req, res, next) {
+  const userId = req.params.id;
+  req.app.locals.db.collection('users').findOne({ _id: userId }, { projection: { password: 0}}).toArray()
+  .then(user => {
+    if (!user) {
+      res.status(404).json({error: 'Användaren kan inte hittas'});
+      return;
+    }
+    res.json(user);
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).json({error: 'Internal Server Error'});
+  })
 });
 
 module.exports = router;
